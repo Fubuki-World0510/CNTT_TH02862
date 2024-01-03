941692394184c519d5b64075db5ed7b43b40c9ee
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-01-03 14:03:59 +0700

    commit change

---
+++ nhapmonlaptrinh/lab/lab1bai3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Toi la : Tran Hai Nam\n");
+    printf("Ngay sinh: 09/08/2007\n");
+    printf("Que quan: Vinh Phuc\n");
+    printf("Noi oi: Ha Noi\n");
+    return 0;
+}
