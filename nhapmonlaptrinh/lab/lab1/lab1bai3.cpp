464ed12e19968c6a45f84eab8a7e19dbf2a3d214
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-01-03 23:37:40 +0700

    delete some shit

--- nhapmonlaptrinh/lab/lab1/lab1bai3.cpp
+++ nhapmonlaptrinh/lab/lab1/lab1bai3.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    printf("Toi la : Tran Hai Nam\n");
+    printf("Toi la: Tran Hai Nam\n");
     printf("Ngay sinh: 09/08/2007\n");
     printf("Que quan: Vinh Phuc\n");
     printf("Noi oi: Ha Noi\n");
