96020568fc9db4ec56fd3aa68fad6d2abac549d1
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-05-08 23:09:22 +0700

    Removed from kỳ 1

--- nhapmonlaptrinh/lab/lab1/lab1bai3.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Toi la: Tran Hai Nam\n");
-    printf("Ngay sinh: 09/08/2007\n");
-    printf("Que quan: Vinh Phuc\n");
-    printf("Noi oi: Ha Noi\n");
-    return 0;
-}
--- nhapmonlaptrinh/ontap/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-if (an < 0)
-            // {
-            //     return printf("[CANH BAO]: BAN DANG NHAP SO NGUYEN AM");
-            // }
