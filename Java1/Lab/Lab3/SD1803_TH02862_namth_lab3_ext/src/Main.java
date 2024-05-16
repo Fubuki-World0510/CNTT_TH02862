5aa423f93565e9b2d1f864ceced8be3b95d68d44
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-05-17 01:13:23 +0700

    Update lab

---
+++ Java1/Lab/Lab3/SD1803_TH02862_namth_lab3_ext/src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SinhVien sv1 = new SinhVien();
+        SinhVien sv2 = new SinhVien();
+        Scanner d = new Scanner(System.in);
+        
+    }
+
+}
--- Java1/Lab/Lab3/SD1803_TH02862_namthth_lab3/src/th02862/bai3.java
+++ Java1/Lab/Lab3/SD1803_TH02862_namthth_lab3/src/th02862/bai3.java
@@ -4,30 +4,43 @@
  */
 package th02862;
 
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
  * @author Administrator
  */
 public class bai3 {
+
     public static void main(String[] args) {
         Scanner cc = new Scanner(System.in);
-        
+
         int[] a = new int[5];
-        
-        for(int i = 0; i < a.length; i++) {
+
+        for (int i = 0; i < a.length; i++) {
             System.out.print("Nhập phần tử số" + i + ":");
             a[i] = cc.nextInt();
         }
         Arrays.sort(a);
-        for(int r : a) {
+        for (int r : a) {
             System.out.println(r);
+        }
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length - 1; i++) {
+                if (a[j] > a[i]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        for (int i : a) {
+            System.out.print("Lớn đến bé" + i);
         }
     }
 }
-
 
 //Sắp xếp từ lớn đến bé, fr
 //for(int i = 0; i < a.length - 1; i++) {
