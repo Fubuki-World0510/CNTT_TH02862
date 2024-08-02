42c9fabb22dae8ff02579f500775e6733a6f9fea
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-08-02 23:11:58 +0700

    Update ass2 js

--- WEB1043/lab/ass2/index.js
+++ WEB1043/lab/ass2/index.js
@@ -1,6 +1,7 @@
 var anhAR = [];
 var currentIndex = 0;
 var invt
+var isLoop = false;
 
 function load() {
     for (var i = 1; i <= 4; i++) {
@@ -10,10 +11,10 @@ function load() {
 }
 
 function tiep() {
-    if (currentIndex < 4) {
+    if (currentIndex <= 4) {
         currentIndex++;
     } else {
-        currentIndex = 0;
+        currentIndex = 1;
     }
     document.getElementById("anh").src = anhAR[currentIndex].src;
 }
@@ -38,11 +39,32 @@ function cuoi() {
 }
 
 function slideShow() {
-   invt = setInterval(() => {
-        tiep();
-    }, 2000);
+    if (!isLoop) {
+        isLoop = true;
+        invt = setInterval(() => {
+            tiep();
+        }, 1000);
+    } else return;
 }
 
 function stop() {
     clearInterval(invt);
 }
+
+document.getElementById("rform").addEventListener("submit", function (event) {
+    var checkboxs = document.querySelectorAll('input[name="checkbox"]:checked');
+    var radios = document.querySelectorAll('input[name="radio"]:checked');
+    if (checkboxs.length == 0) {
+        alert("Vui lòng chọn sở thích");
+        event.preventDefault();
+    }
+    if (radios.length == 0) {
+        alert("Vui lòng chọn giới tính");
+        event.preventDefault();
+    }
+    if (checkboxs.length > 0 && radios.length > 0) {
+        console.log("Sở thích:", Array.from(checkboxs).map(cb => cb.id));
+        console.log("Giới tính:", radios[0].value);
+        event.preventDefault();
+    }
+});
